02fd8831dc6f4b97f85ea4d036a6bd27e87c6c06
OriginalGamer <htorrisi@hotmail.com>
2015-03-09 16:30:19 +1000

    Setting up menu

---
+++ Trove of Thieves/Assets/Scripts/gUI/s_MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class s_MainMenu : MonoBehaviour {
+
+	public Button butt;
+
+	// Use this for initialization
+	void Start () {
+		butt = butt.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+
+	}
+
+	public void OnButtTouch(){
+	
+	}
+}
