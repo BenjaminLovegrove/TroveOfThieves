e42e40b6253480a06b9d4ef4642063e6ff0cb1a4
Benjamin <Benjamin_JL@live.com>
2015-03-11 13:02:02 +1000

    Added icon fade script - didnt apply

---
+++ Trove of Thieves/Assets/Scripts/gUI/s_CostFade.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class s_CostFade : MonoBehaviour {
+
+	public s_EventManager EventManager;
+	public Button thisButton;
+
+	public int cost;
+
+	void Start(){
+		thisButton = GetComponent<Button>();
+		EventManager = (s_EventManager)GameObject.Find ("EventManager").GetComponent <s_EventManager> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+		if (EventManager.playerTurnToken == 1) {
+			if (EventManager.playerOneAP > cost){
+				thisButton.interactable = true;
+			} else {
+				thisButton.interactable = false;
+			}
+		}
+
+		if (EventManager.playerTurnToken == 2) {
+			if (EventManager.playerTwoAP > cost){
+				thisButton.interactable = true;
+			} else {
+				thisButton.interactable = false;
+			}
+		}
+
+	}
+}
