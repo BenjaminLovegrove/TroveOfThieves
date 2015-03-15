eb4c846c667b0b3d765d7e31a13bb263a700492e
Benjamin <Benjamin_JL@live.com>
2015-03-15 11:49:19 +1000

    Fixed thieves moving on y axis while animating

---
+++ Trove of Thieves/Assets/Scripts/Theif/s_AnimMovementFix.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_AnimMovementFix : MonoBehaviour {
+
+	float setY;
+
+	// Use this for initialization
+	void Start () {
+		setY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (this.transform.position.x, setY, this.transform.position.z);
+	}
+}
